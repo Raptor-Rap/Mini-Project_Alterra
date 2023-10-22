9d5da14c5176dfb2e9d03c90cef623f838bed65c
Raptor-Rap <rmfikri42@gmail.com>
2023-10-22 22:33:14 +0700

    refactor

--- travelo-app/src/components/layout.jsx
+++ travelo-app/src/components/layout.jsx
@@ -1,11 +1,30 @@
+import { ToastContainer } from "react-toastify";
 import Navbar from "./navbar";
 import Footer from "./footer";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
+
+import "react-toastify/dist/ReactToastify.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "animate.css";
 
 export default function Layout({ children }) {
   return (
     <div>
       <Navbar />
-      <div>{children}</div>
+      <div>
+        {children}
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={true}
+          closeOnClick
+          draggable={false}
+          pauseOnHover
+        />
+      </div>
       <Footer />
     </div>
   );
